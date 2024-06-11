a43a151125f570dd399e437a79c443eaeda7d975
Dinioktivani <2200016074@webmail.uad.ac.id>
2024-06-11 22:02:33 +0700

    prak9

---
+++ prak9/lib/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const kShrinePink50 = Color.fromARGB(255, 250, 158, 140);
+const kShrinePink100 = Color(0xFFFEDBD0);
+const kShrinePink300 = Color.fromARGB(255, 11, 7, 6);
+const kShrinePink400 = Color(0xFFEAA4A4);
+
+const kShrineBrown900 = Color(0xFF442B2D);
+
+const kShrineErrorRed = Color(0xFFC5032B);
+
+const kShrineSurfaceWhite = Color(0xFFFFFBFA);
+const kShrineBackgroundWhite = Colors.white;
